9dcbcf14ef5268e350f1e2182da4509fe1bdf3a4
JoshuaHumpz <39768780+JoshuaHumpz@users.noreply.github.com>
2018-06-03 07:42:36 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START", "TYPING_STOP", "GUILD_SYNC", "RELATIONSHIP_ADD", "RELATIONSHIP_REMOVE", "USER_SETTINGS_UPDATE", "USER_NOTE_UPDATE"], reconnect: true });
 const { readdirSync } = require("fs");
 
 const startBot = async () => {
